95740d5e6120370dd3f1eafa43995106d034bf3a
Andrei1892 <andreipiti17@gmail.com>
2019-03-05 08:48:08 +0200

    structured pagination

---
+++ movies/src/common/Pagination.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Pagination = props => {
+    const { crtPage , maxItems , pageSize , onPageChange } = props;  
+    return(
+          <nav>
+                <ul className="pagination">
+                    { pages.map(page => 
+                        <li key={page} 
+                        className="page-item">
+                        <a className="page-link"  
+                        onClick={ () => onPageChange }>{crtPage}</a>   
+                        </li>
+                    )}
+                </ul>
+          </nav>
+        )
+}
+export default Pagination;
